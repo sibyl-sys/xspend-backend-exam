6bb723af1b90d8f639d8966d106a7335c6bf6b71
mcanjelo <mcanjelo29@gmail.com>
2021-12-17 09:56:27 +0800

    Add route for getting wallet balance

--- routes/walletRouter.ts
+++ routes/walletRouter.ts
@@ -14,4 +14,14 @@ walletRouter.post("/create", async (req: Request, res: Response) => {
     })
 });
 
+walletRouter.get("/read/:wallet_address", async (req: Request, res: Response) => {
+    walletModel.findWallet(req.params.wallet_address, (err: Error, wallet: Wallet) => {
+        if (err) {
+            return res.status(500).json({"message": err.message});
+        }
+
+        res.status(200).json({"balance": wallet.balance});
+    })
+});
+
 export {walletRouter};
