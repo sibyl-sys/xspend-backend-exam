d8165a99a8aa5d5564681b97a4fac8539473b8e6
mcanjelo <mcanjelo29@gmail.com>
2021-12-17 10:36:17 +0800

    Add wallet validation

--- routes/walletRouter.ts
+++ routes/walletRouter.ts
@@ -2,47 +2,64 @@ import express, {Request, Response} from "express";
 import * as walletModel from "../models/wallet";
 import {Wallet} from "../types/wallet";
 const walletRouter = express.Router();
+const Web3 = require('web3');
 
 walletRouter.post("/create", async (req: Request, res: Response) => {
     const newWallet: Wallet = req.body;
-    walletModel.create(newWallet, (err: Error, insertID: number) => {
-        if (err) {
-            return res.status(500).json({"message": err.message});
-        }
-
-        res.status(200).json({"insertID": insertID});
-    })
+    if(Web3.utils.isAddress(newWallet.wallet_address)) {
+        walletModel.create(newWallet, (err: Error, insertID: number) => {
+            if (err) {
+                return res.status(500).json({"message": err.message});
+            }
+    
+            res.status(200).json({"insertID": insertID});
+        })
+    } else {
+        res.status(500).json({"message": "Invalid wallet address"});
+    }
 });
 
 walletRouter.get("/read/:wallet_address", async (req: Request, res: Response) => {
-    walletModel.findWallet(req.params.wallet_address, (err: Error, wallet: Wallet) => {
-        if (err) {
-            return res.status(500).json({"message": err.message});
-        }
+    if(Web3.utils.isAddress(req.params.wallet_address)) {
+        walletModel.findWallet(req.params.wallet_address, (err: Error, wallet: Wallet) => {
+            if (err) {
+                return res.status(500).json({"message": err.message});
+            }
 
-        res.status(200).json({"balance": wallet.balance});
-    })
+            res.status(200).json({"balance": wallet.balance});
+        })
+    } else {
+        res.status(500).json({"message": "Invalid wallet address"});
+    }
 });
 
 walletRouter.put("/update/", async (req: Request, res: Response) => {
     const existingWallet: Wallet = req.body;
-    walletModel.update(existingWallet, (err: Error, wallet: Wallet) => {
-        if (err) {
-            return res.status(500).json({"message": err.message});
-        }
+    if(Web3.utils.isAddress(existingWallet.wallet_address)) {
+        walletModel.update(existingWallet, (err: Error, wallet: Wallet) => {
+            if (err) {
+                return res.status(500).json({"message": err.message});
+            }
 
-        res.status(200).send();
-    })
+            res.status(200).send();
+        })
+    } else {
+        res.status(500).json({"message": "Invalid wallet address"});
+    }
 });
 
 walletRouter.delete("/delete/:wallet_address", async (req: Request, res: Response) => {
-    walletModel.deleteWallet(req.params.wallet_address, (err: Error, wallet: Wallet) => {
-        if (err) {
-            return res.status(500).json({"message": err.message});
-        }
+    if(Web3.utils.isAddress(req.params.wallet_address)) {
+        walletModel.deleteWallet(req.params.wallet_address, (err: Error, wallet: Wallet) => {
+            if (err) {
+                return res.status(500).json({"message": err.message});
+            }
 
-        res.status(200).send();
-    })
+            res.status(200).send();
+        })
+    } else {
+        res.status(500).json({"message": "Invalid wallet address"});
+    }
 })
 
 export {walletRouter};
